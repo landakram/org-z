3470fd930fa3c9e55f28345cf94d50ad40a61466
Mark Hudnall <me@markhudnall.com>
2020-11-28 00:56:31 -0800

    Fix package-lint

--- org-z.el
+++ org-z.el
@@ -1,13 +1,30 @@
-;;; org-z.el -- Utilities for linking in org-mode -*- coding: utf-8; lexical-binding: t; -*-
+;;; org-z.el --- Lightweight, Org-mode flavored zettelkasten links -*- coding: utf-8; lexical-binding: t; -*-
 
-;; Copyright (C) 2020 Mark Hudnall
+;; Copyright (C) 2020 Mark Hudnall <me@markhudnall.com>
 
 ;; Author: Mark Hudnall <me@markhudnall.com>
 ;; URL: https://github.com/landakram/org-z
 ;; Keywords: org-mode
-;; Package-Version: 0.0.1
-;; Package-Requires: ((emacs "26.1") (org "9.3") (helm-org-rifle "1.7.1") (helm-rg "0.1") (dash "2.12") (f "0.18.1") (s "1.10.0"))
-;; Keywords: (outlines)
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "27.1") (org "9.3") (helm-org-rifle "1.7.1") (helm-rg "0.1") (dash "2.12") (f "0.18.1") (s "1.10.0"))
+;; Keywords: org-mode, outlines
+
+;; This file is NOT part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
 
 ;;; Commentary:
 
